63d920fb74282ce9fc297f5483e4e73559268aea
Frederik Bosch <frede.bosch@gmail.com>
2017-05-22 23:56:20 +0200

    ci: added keys.js

---
+++ src/keys.js
@@ -0,0 +1,9 @@
+export default {
+    discogs: {
+        secret: 'xESJAUIVlIOyhsonGZBXLBtYuUfwlAOT',
+        key: 'NaaCDTcomZsvghNVsJIo'
+    },
+    lastfm: {
+        key: '81cbdf705ac4dc49c0a55b48bee878c9'
+    }
+}
