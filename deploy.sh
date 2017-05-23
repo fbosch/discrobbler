c6ce7a1cc15380d3da264107f60060a6c7d554ef
fBosch <frede.bosch@gmail.com>
2017-05-23 15:29:35 +0200

    ci: automatic deploy to gh-pages
    implemented automatic deploy to gh-pages on master commit

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
-# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
-set -o errexit
 
 rm -rf public
 mkdir public
 
-# config
-git config --global user.email "nobody@nobody.org"
-git config --global user.name "Travis CI"
-
 # build
 yarn run build
-cp index.html public
+sed -e 's/base href="\/\"/base href="\/vue-discogs-scrobbler"/g' index.html > public/index.html
 cp -r dist public 
-
-# deploy
-cd public
-git init
-git add .
-git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
