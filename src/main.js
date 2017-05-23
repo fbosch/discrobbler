b29f1e2d30929098eea5a9fe7e486d27037480b9
fBosch <frede.bosch@gmail.com>
2017-05-23 22:37:57 +0200

    style: moved firebase init out of index.html

--- src/main.js
+++ src/main.js
@@ -7,6 +7,20 @@ import material from 'vue-material'
 import lazyload from 'vue-lazyload'
 import ProgressiveImage from 'progressive-image/dist/vue'
 
+import 'firebase/auth'
+import 'firebase/database'
+import firebase from 'firebase/app'
+
+var config = {
+  apiKey: "AIzaSyBFIvEfsNsjaHfMmbQCadB8nWWegfXIIVo",
+  authDomain: "discogs-scrobbler.firebaseapp.com",
+  databaseURL: "https://discogs-scrobbler.firebaseio.com",
+  projectId: "discogs-scrobbler",
+  storageBucket: "discogs-scrobbler.appspot.com",
+  messagingSenderId: "397731299445"
+};
+firebase.initializeApp(config);
+
 Vue.use(material)
 Vue.use(lazyload)
 Vue.use(ProgressiveImage, {
