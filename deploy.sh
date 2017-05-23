19080f537c3e018843908a297612352182592d40
fBosch <frede.bosch@gmail.com>
2017-05-23 22:27:13 +0200

    installed firebase

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ mkdir public
 
 # build
 yarn run build
-sed -e 's/base href="\/\"/base href="\/vue-discogs-scrobbler\/"/g' index.html > public/index.html
+cp index.html public/index.html
 cp -r dist public 
