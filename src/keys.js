117ff2d46df19c956ac0f64a5e2f01d861582087
fBosch <frede.bosch@gmail.com>
2017-05-23 22:52:50 +0200

    changed api keys

---
+++ src/keys.js
@@ -0,0 +1,9 @@
+export default {
+    discogs: {
+        key: 'ycpbsHHKdWKsqmoJJgfs', 
+        secret: 'UfhepNGDotfmesBNVVByeOAyLhMOsOgb'
+    },
+    lastfm: {
+        key: '9f864ab4c5f961229de13bdc4b552ff7',
+    }
+}
